a564d479a5e64b4cf9ca4ad7e8ffc5cd602a1418
TimiBolaji <mattybolaji@gmail.com>
2025-02-12 18:53:11 +0100

    ft: updated routes

--- src/routes/orphanageRoutes.js
+++ src/routes/orphanageRoutes.js
@@ -1,9 +1,15 @@
 const express = require ("express")
 const orphanageRoutes = express.Router()
 const orphanageController = require ("../controllers/orphanageController")
+const expenseController = require ("../controllers/expenseController")
 
-orphanageRoutes.post('/orphanage', orphanageController.addOrphanage)
-orphanageRoutes.get('/orphanages', orphanageController.getAllOrphanages)
-orphanageRoutes.get('/orphanages/:id', orphanageController.getOrphanageDetails)
+
+orphanageRoutes.post('/', orphanageController.addOrphanage)
+orphanageRoutes.get('/', orphanageController.getAllOrphanages)
+orphanageRoutes.get('/:id', orphanageController.getOrphanageDetails)
+orphanageRoutes.post('/expenses', expenseController.addExpense)
+orphanageRoutes.get('/expenses/:orphanageId', expenseController.getExpensesForOrphanage)
+
+
 
 module.exports = orphanageRoutes
